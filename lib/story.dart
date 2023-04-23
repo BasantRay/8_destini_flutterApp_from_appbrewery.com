e5d4829f5e2818e8da93ba78d61018d7fb002ad0
BasantRay <basantray344@gmail.com>
2023-04-23 09:19:42 +0545

    first commit

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+import 'story_brain.dart';
+
+class Story{
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
+
+}
